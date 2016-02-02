f85c35b1d63fe30ea3cb8cd353afd9cff1d0a5b2
Aldo Cortesi <aldo@nullcube.com>
2016-02-03 09:20:42 +1300

    Attribution and license for parser and lexer strategies
    The base for these modules is from Go's text/template

--- conf/lex.go
+++ conf/lex.go
@@ -1,5 +1,11 @@
 package conf
 
+// The base lexer is from the text/template package in Go.
+
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import (
 	"fmt"
 	"strings"
