e87899cfe8f146357d4e5495d9eab24a435c5515
Duncan Holm <duncan@holm.scot>
2017-11-28 09:09:01 +0000

    Fill out struct initializer in posix tests too

--- conf/parse_posix_test.go
+++ conf/parse_posix_test.go
@@ -12,15 +12,15 @@ var parsePosixTests = []struct {
 }{
 	{
 		"{\ndaemon +sigusr1: c\n}",
-		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGUSR1}}}}},
+		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGUSR1, false}}}}},
 	},
 	{
 		"{\ndaemon +sigusr2: c\n}",
-		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGUSR2}}}}},
+		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGUSR2, false}}}}},
 	},
 	{
 		"{\ndaemon +sigwinch: c\n}",
-		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGWINCH}}}}},
+		&Config{Blocks: []Block{{Daemons: []Daemon{{"c", syscall.SIGWINCH, false}}}}},
 	},
 }
 
